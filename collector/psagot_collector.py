73094c039a11666fa359a8ee1ea6e3a921f30457
Michael Palarya <michael.palarya@redislabs.com>
2020-06-22 00:12:30 +0300

    make mypy stricter

--- collector/psagot_collector.py
+++ collector/psagot_collector.py
@@ -1,10 +1,14 @@
 import datetime
+from datetime import datetime as dt
 import logging
+from logging import Logger
+from typing import List, Dict
 from urllib.parse import urljoin
 import requests
+from requests import PreparedRequest, Response
 
 
-def get_logger(logger_name):
+def get_logger(logger_name: str) -> Logger:
     logger = logging.getLogger(logger_name)
     stream_handler = logging.StreamHandler()
     formatter = logging.Formatter("[%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]")
@@ -19,23 +23,17 @@ class PsagotCollector:
     BASE_URL = "https://sparkpsagot.ordernet.co.il/api/"
     DATETIME_FORMAT = "%Y-%m-%dT%H:00:00.000Z"
 
-    def __init__(self, username, password, account_number, logger=None):
+    def __init__(self, username: str, password: str, account_number: str, logger: Logger = None):
         self.username = username
         self.password = password
-        self.log = get_logger(self.__class__.__name__) if logger is None else logger
+        self.log = logger or get_logger(self.__class__.__name__)
         self.account_number = account_number
 
-    def send_request(self, prepared_request):
+    def send_request(self, prepared_request: PreparedRequest) -> Response:
         session = requests.Session()
-        response = session.send(request=prepared_request)
+        return session.send(request=prepared_request)
 
-        try:
-            return response.json()
-
-        except Exception as e:
-            self.log.error(e)
-
-    def _request_authenticate(self):
+    def _request_authenticate(self) -> PreparedRequest:
         auth_end_point = "Auth/Authenticate"
         auth = {"username": self.username, "password": self.password}
         request = requests.Request("POST", urljoin(self.BASE_URL, auth_end_point), json=auth)
@@ -44,7 +42,7 @@ class PsagotCollector:
 
         return request.prepare()
 
-    def _request_data(self, access_token, start_date, end_date):
+    def _request_data(self, access_token: str, start_date: str, end_date: str) -> PreparedRequest:
         transactions_end_point = "Account/GetAccountTransactions"
 
         request = requests.Request("GET", urljoin(self.BASE_URL, transactions_end_point))
@@ -57,17 +55,19 @@ class PsagotCollector:
 
         return request.prepare()
 
-    def authenticate(self):
-        return self.send_request(self._request_authenticate())
+    def authenticate(self) -> Dict[str, str]:
+        return self.send_request(self._request_authenticate()).json()
 
-    def get_data(self, start_date=None, end_date=None):
+    def get_data(self, start_date: dt = None, end_date: dt = None) -> List[Dict[str, str]]:
         if not start_date:
-            start_date = datetime.datetime.now() - datetime.timedelta(days=3 * 365)
+            start_date = dt.now() - datetime.timedelta(days=3 * 365)
         if not end_date:
-            end_date = datetime.datetime.now()
+            end_date = dt.now()
         access_token = self.authenticate().get("l")
+        if access_token is None:
+            raise AttributeError("Authentication response is invalid")
 
-        transactions = []
+        transactions: List[Dict[str, str]] = []
         while end_date.year - start_date.year > 0:
             tmp_end_date = end_date.replace(year=start_date.year, month=12, day=31)
 
@@ -78,7 +78,7 @@ class PsagotCollector:
                         start_date.strftime(self.DATETIME_FORMAT),
                         tmp_end_date.strftime(self.DATETIME_FORMAT),
                     )
-                )
+                ).json()
             )
 
             start_date = start_date.replace(year=start_date.year + 1, month=1, day=1)
@@ -88,7 +88,7 @@ class PsagotCollector:
                 self._request_data(
                     access_token, start_date.strftime(self.DATETIME_FORMAT), end_date.strftime(self.DATETIME_FORMAT)
                 )
-            )
+            ).json()
         )
 
         return transactions
